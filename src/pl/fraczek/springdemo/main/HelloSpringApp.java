2d4a18cce3ca1de159e2326fd1e423ca0fbb3681
jakubfraczek <kuba.fraczek@gmail.com>
2018-12-06 00:09:31 +0100

    Spring Inversion of control

---
+++ src/pl/fraczek/springdemo/main/HelloSpringApp.java
@@ -0,0 +1,25 @@
+package pl.fraczek.springdemo.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.fraczek.springdemo.coaches.Coach;
+
+/**
+ * @author <a href=mailto:Jakub.Fraczek@tomtom.com>Jakub Fraczek</a>
+ */
+public class HelloSpringApp {
+
+    public static void main(String[] args) {
+
+        // load the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        final Coach coach = context.getBean("myCoach", Coach.class);
+
+        // call methods on the bean
+        System.out.println(coach.getDailyWorkout());
+
+        // close context
+        context.close();
+    }
+}
