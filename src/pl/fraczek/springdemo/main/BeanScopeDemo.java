bfbff5e681519c29c9403b4e4b55e1803d139fd9
jakubfraczek <kuba.fraczek@gmail.com>
2018-12-10 15:57:52 +0100

    Beans scope exercises

---
+++ src/pl/fraczek/springdemo/main/BeanScopeDemo.java
@@ -0,0 +1,32 @@
+package pl.fraczek.springdemo.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.fraczek.springdemo.coaches.Coach;
+
+/**
+ * @author <a href=mailto:Jakub.Fraczek@tomtom.com>Jakub Fraczek</a>
+ */
+public class BeanScopeDemo {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("scopeBean-applicationContext.xml");
+
+        final Coach myCoach = context.getBean("myCoach", Coach.class);
+        final Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        final boolean myCoachResult = myCoach == alphaCoach;
+        System.out.println(myCoachResult);
+        System.out.println("Memory reference for myCoach: " + myCoach);
+        System.out.println("Memory reference for alphaCoach: " + alphaCoach);
+
+        final Coach yourCoach = context.getBean("yourCoach", Coach.class);
+        final Coach betaCoach = context.getBean("yourCoach", Coach.class);
+
+        final boolean yourCoachResult = yourCoach == alphaCoach;
+        System.out.println(yourCoachResult);
+        System.out.println("Memory reference for yourCoach: " + myCoach);
+        System.out.println("Memory reference for betaCoach: " + betaCoach);
+
+    }
+}
