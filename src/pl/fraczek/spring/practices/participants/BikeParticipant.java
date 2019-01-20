2b4f7066e5f421c42ebcf5548e6a58223efaf63f
jakubfraczek <kuba.fraczek@gmail.com>
2019-01-20 12:48:02 +0100

    Fix for BikeParticipant

--- src/pl/fraczek/spring/practices/participants/BikeParticipant.java
+++ src/pl/fraczek/spring/practices/participants/BikeParticipant.java
@@ -2,7 +2,7 @@ package pl.fraczek.spring.practices.participants;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import pl.fraczek.spring.demo.fortune.FortuneService;
+import pl.fraczek.spring.practices.fortune.FortuneService;
 
 /**
  * @author <a href=mailto:Jakub.Fraczek@tomtom.com>Jakub Fraczek</a>
@@ -14,6 +14,7 @@ public class BikeParticipant implements Participant {
     private String teamName;
 
     public BikeParticipant(FortuneService fortuneService, final String email, final String teamName) {
+
         this.fortuneService = fortuneService;
         this.email = email;
         this.teamName = teamName;
@@ -21,28 +22,34 @@ public class BikeParticipant implements Participant {
 
     @Override
     public String myDailyPractice() {
+
         return "Bike 100 km!";
     }
 
     @Autowired
     @Qualifier("fileFortuneService")
     public String myDailyFortune() {
+
         return fortuneService.getFortune();
     }
 
     public void hello() {
+
         System.out.println("Welcome into the training!");
     }
 
     public void goodbye() {
+
         System.out.println("Thaks! Have a nice day!");
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public String getTeamName() {
+
         return teamName;
     }
 }
