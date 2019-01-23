3da3e5b6a4638412ab6b8460606c7906fd665d05
jakubfraczek <kuba.fraczek@gmail.com>
2019-01-23 21:50:27 +0100

    [Practice] Properties injection using java annotations

--- src/pl/fraczek/spring/practices/main/AnnotationSpringAppPractice.java
+++ src/pl/fraczek/spring/practices/main/AnnotationSpringAppPractice.java
@@ -2,6 +2,7 @@ package pl.fraczek.spring.practices.main;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.fraczek.spring.practices.participants.Participant;
+import pl.fraczek.spring.practices.participants.SwimParticipant;
 
 /**
  * @author <a href=mailto:Jakub.Fraczek@tomtom.com>Jakub Fraczek</a>
@@ -12,8 +13,14 @@ public class AnnotationSpringAppPractice {
 
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("pl/fraczek/spring/practices/configuration/myPracticeAnnotation-applicationContext.xml")) {
             final Participant skiParticipant = context.getBean("skiParticipant", Participant.class);
+            System.out.println("This is skiParticipant");
             System.out.println(skiParticipant.myDailyPractice());
             System.out.println(skiParticipant.myDailyFortune());
+
+            final SwimParticipant swimParticipant = context.getBean("swimParticipant", SwimParticipant.class);
+            System.out.println("\n\nThis is swimParticipant");
+            System.out.println("Mail: " + swimParticipant.getEmail());
+            System.out.println("Team name: " + swimParticipant.getTeamName());
         }
 
     }
--- src/pl/fraczek/spring/practices/participants/SwimParticipant.java
+++ src/pl/fraczek/spring/practices/participants/SwimParticipant.java
@@ -2,6 +2,7 @@ package pl.fraczek.spring.practices.participants;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.fraczek.spring.practices.fortune.FortuneService;
 
@@ -9,20 +10,38 @@ import pl.fraczek.spring.practices.fortune.FortuneService;
  * @author <a href=mailto:Jakub.Fraczek@tomtom.com>Jakub Fraczek</a>
  */
 @Component
-public class SkiParticipant implements Participant {
+public class SwimParticipant implements Participant {
 
     @Autowired
-    @Qualifier("fileFortuneService")
+    @Qualifier("randomFortune")
     private FortuneService fortuneService;
 
+    @Value("${swim.mail}")
+    private String email;
+
+    @Value("${swim.mail}")
+    private String teamName;
+
     @Override
+
     public String myDailyPractice() {
-        return "Do something!";
+
+        return "Swim something or whatever";
     }
 
     @Override
     public String myDailyFortune() {
+
         return fortuneService.getFortune();
     }
 
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getTeamName() {
+
+        return teamName;
+    }
 }
